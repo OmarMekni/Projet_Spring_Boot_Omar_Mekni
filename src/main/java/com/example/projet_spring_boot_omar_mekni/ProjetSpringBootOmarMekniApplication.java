e01bd9d0649d3a045e93e0551718f5bdffc8024a
omar <omar@gmail.com>
2024-01-05 15:39:16 -0500

    commit initial

---
+++ src/main/java/com/example/projet_spring_boot_omar_mekni/ProjetSpringBootOmarMekniApplication.java
@@ -0,0 +1,13 @@
+package com.example.projet_spring_boot_omar_mekni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetSpringBootOmarMekniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetSpringBootOmarMekniApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projet_spring_boot_omar_mekni/ProjetSpringBootOmarMekniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projet_spring_boot_omar_mekni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetSpringBootOmarMekniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
