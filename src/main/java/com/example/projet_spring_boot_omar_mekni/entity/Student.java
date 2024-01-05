eaf37f092e590b3c03a60b521ea4561e8e6dea0a
omar <omar@gmail.com>
2024-01-05 15:54:17 -0500

    ajout de la class StudentControleur

--- src/main/java/com/example/projet_spring_boot_omar_mekni/entity/Student.java
+++ src/main/java/com/example/projet_spring_boot_omar_mekni/entity/Student.java
@@ -1,4 +1,25 @@
 package com.example.projet_spring_boot_omar_mekni.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String prenom;
+    private String nom;
+    private String specialite;
+
+
+
 }
