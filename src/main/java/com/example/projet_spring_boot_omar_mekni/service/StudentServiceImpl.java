4fa7e09913d5e18548843670a0b5427cde58e214
omar <omar@gmail.com>
2024-01-05 16:02:49 -0500

    ajout de la class StudentServiceImpl

---
+++ src/main/java/com/example/projet_spring_boot_omar_mekni/service/StudentServiceImpl.java
@@ -0,0 +1,27 @@
+package com.example.projet_spring_boot_omar_mekni.service;
+
+import com.example.projet_spring_boot_omar_mekni.entity.Student;
+
+import java.util.List;
+
+public class StudentServiceImpl implements StudentService {
+    @Override
+    public Student saveStudent(Student student) {
+        return null;
+    }
+
+    @Override
+    public Student findStudentById(int id) {
+        return null;
+    }
+
+    @Override
+    public boolean deleteStudentById(int id) {
+        return false;
+    }
+
+    @Override
+    public List<Student> findAll() {
+        return null;
+    }
+}
